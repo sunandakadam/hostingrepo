1a10e48b56cc46542c10aa1736bb7b8957ad1b48
Sunanda Kadam <skadam@SAGASOFT.COM>
2020-07-18 12:41:02 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'jspm-rscoe';
+export class AppComponent implements OnInit {
+  topics=['Angular','React','Vue'];
+  
+  ngOnInit(): void {
+   
+  }
+  onSubmit(form1){
+    console.log('username',form1.controls['uname'].value);
+  }
 }
